0a07b318a780373f86796580adf027b20befbafa
jbratcher <jeremybratcher@gmail.com>
2019-11-06 18:07:24 -0500

    initial commit

---
+++ Controllers/FriendGroupInstanceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FriendGroup.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FriendGroupInstanceController : ControllerBase
+    {
+        private readonly ILogger<FriendGroupInstanceController> _logger;
+
+        public FriendGroupInstanceController(ILogger<FriendGroupInstanceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<FriendGroupInstance> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new FriendGroupInstance
+            {
+                GroupId = rng.Next(1, 10),
+                GroupName = "Group " + rng.Next(1, 10),
+                GroupMembers = new string[4] { "Jim", "Pam", "Michael", "Dwigt"}
+            }
+            )
+            .ToArray();
+        }
+    }
+}
---
+++ FriendGroupInstance.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace FriendGroup
+{
+    public class FriendGroupInstance
+    {
+        public string Summary { get; set; }
+
+        public int GroupId { get; set; }
+
+        public string GroupName { get; set; }
+
+        public string[] GroupMembers { get; set; }
+    }
+}
